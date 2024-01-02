100acd0a9928c2b9e162e3e069d97f8d6c778e57
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2024-01-02 09:57:12 -0300

    stilização input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,43 +28,52 @@ export default function Home() {
    const canvasRef = useRef<ReactSketchCanvasRef>(null);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form
-        className=" flex flex-col gap-4"
-        onSubmit={handleSubmit(async (formData) => {
-          if (!canvasRef.current) return;
-          const image = await canvasRef.current.exportImage("jpeg");
-          console.log(image, "imagem do desenho");
-          const result = await salvarMutation({ ...formData, image });
-          console.log(result);
-        })}>
-        <input
-          className=" text-black"
-          {...register("prompt", { required: true })}
-        />
-        {errors.prompt && <span>This field is required</span>}
-        <ReactSketchCanvas
-          ref={canvasRef}
-          style={{ width: 256, height: 256 }}
-          strokeWidth={4}
-          strokeColor="black"
-        />
-        <input className=" rounded bg-red-700" type="submit" />
-
-        <section>
-          <h2>Recent Sketches</h2>
-          <div className="grid grid-cols-4 gap-4">
-            {sortedSketches.map((sketch) => (
-              <img
-                key={sketch._id}
-                width="256"
-                height="256"
-                src={sketch.result}
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-50">
+      <div className="">
+        <form
+          className=" flex flex-col gap-2"
+          onSubmit={handleSubmit(async (formData) => {
+            if (!canvasRef.current) return;
+            const image = await canvasRef.current.exportImage("jpeg");
+            console.log(image, "imagem do desenho");
+            const result = await salvarMutation({ ...formData, image });
+            console.log(result);
+          })}>
+          <div className=" flex items-center flex-col gap-4">
+            <input
+              className="text-black border-2 border-black-500 px-2 py-1 shadow-md"
+              {...register("prompt", { required: true })}
+            />
+            {errors.prompt && <span>This field is required</span>}
+            <div className="text-black border-2 border-black-500 px-2 py-1 shadow-md">
+              <ReactSketchCanvas
+                ref={canvasRef}
+                style={{ width: 256, height: 256 }}
+                strokeWidth={4}
+                strokeColor="black"
               />
-            ))}
+            </div>
+            <input
+              className=" rounded bg-green-700 shadow-md w-60 h-8"
+              type="submit"
+            />
           </div>
-        </section>
-      </form>
+          <section>
+            <h2>Imagens Criadas</h2>
+
+            <div className="grid grid-cols-4 gap-4">
+              {sortedSketches.map((sketch) => (
+                <img
+                  key={sketch._id}
+                  width="256"
+                  height="256"
+                  src={sketch.result}
+                />
+              ))}
+            </div>
+          </section>
+        </form>
+      </div>
     </main>
   );
 }
